64e6eff81ca745acf6d306899a9740171031ae9d
Diego Galindo <diegogalindo84@gmail.com>
2022-05-22 19:04:15 -0300

    Test1

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ public class Program {
 		System.out.println("Hello world");
 		System.out.println("Hello world");
 		System.out.println("Hello world");
-		System.out.println("Hello world");
+		
 		System.out.println("Hello world");
 	}
 
